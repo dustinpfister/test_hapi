6636d9c11f3d3533052435eb71c4dfeace8ffaf5
dustinpfister <dustin.pfister@gmail.com>
2017-09-27 17:33:41 -0400

    demo update

--- demos/hello_world.js
+++ demos/hello_world.js
@@ -1,27 +1,27 @@
 var Hapi = require('hapi');
 
 // create a new instance of hapi server
-var ser = new Hapi.Server();
+var server = new Hapi.Server();
 
 // port 3000, and I will be using localhost
 // when running I will connect via http://localhost:3000
-ser.connection({ port: 3000, host: 'localhost' });
+server.connection({ port: 3000, host: 'localhost' });
 
 
 // just one route for now
-ser.route({
+server.route({
     method: 'GET',
     path: '/',
-    handler: function (req, res) {
+    handler: function (request, reply) {
 
-        res('hapi! hapi! hapi!...joy! joy! joy!');
+        reply('hapi! hapi! hapi!...joy! joy! joy!');
     }
 });
 
 
 // start the server
-ser.start(function(){
+server.start(function(){
 
-    console.log(`hapi server up: ${ser.info.uri}`);
+    console.log('hapi server up!');
 
 });
